12fbf505a1d737e4e897080f3db788949dde26ee
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-11 13:46:13 -0300

    feat: create gitignore

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import tarefaRoutes from "./routes/tarefaRoutes.js";
+
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 app.use("/tarefas", tarefaRoutes);
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
