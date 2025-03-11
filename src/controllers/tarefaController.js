5310bc9281ecc4b7816c6a8fe07eb6277faf1606
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-11 16:14:24 -0300

    feat: add create method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -10,13 +10,21 @@ class TarefaController {
       res.status(500).json({ erro: "Erro ao buscar tarefas." })
     }
   };
-  create = ({ body: { descricao } }, res) => {
-    if (!descricao) {
-      return res.status(400).json({ erro: "Descrição é obrigatória" });
+
+  create = async  (req, res) => {
+    const { descricao } = req.body;
+    try{    
+      if (!descricao) {
+        return res.status(400).json({ erro: "Descrição é obrigatória" });
+      }
+      const novaTarefa = tarefaModel.create(descricao);
+      res.status(201).json(novaTarefa);
+    } catch (error){
+        console.log(error);
+        res.status(500).json({ erro:"Erro ao criar tarefa" })
     }
-    const novaTarefa = tarefaModel.create(descricao);
-    res.status(201).json(novaTarefa);
   };
+
   update = ({ params: { id }, body: { concluida } }, res) => {
     const tarefaAtualizada = tarefaModel.update(id, concluida);
     if (!tarefaAtualizada) {
